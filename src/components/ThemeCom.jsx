110395de5fd999d1012d87f241e38f76725de0d1
Adrian Netto <adrian.netto73@gmail.com>
2025-02-06 17:45:19 -0300

    feat: adding theme changer

---
+++ src/components/ThemeCom.jsx
@@ -0,0 +1,29 @@
+"use client";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+export default function ThemeCom({ children }) {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  useEffect(() => {
+    console.log("Mounted:", mounted);
+    console.log("Theme:", theme);
+  }, [mounted, theme]);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <div className={theme}>
+      <div className="bg-white text-gray-700 dark:text-gray-200 dark:bg-gray-900 min-h-screen">
+        {children}
+      </div>
+    </div>
+  );
+}
