19ee461052e936b683f3e7c359d0b7dbfcd0dfe4
Adrian Netto <adrian.netto73@gmail.com>
2025-02-10 08:35:08 -0300

    fix: correct import

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/header";
+import Header from "@/components/Header";
 import { ThemeProvider } from "next-themes";
 import ThemeCom from "@/components/ThemeCom";
 import { ClerkProvider } from "@clerk/nextjs";
